9ecd1ecf61db76a6d929be560def8ada88228d08
Mayur Jadhav <mayur.jadhav@mapsted.com>
2024-12-31 19:40:14 +0530

    Added Effects on Tshirt section

--- components/Sections/TshirtSection.jsx
+++ components/Sections/TshirtSection.jsx
@@ -1,29 +1,33 @@
 import React from "react";
 import SingleProduct from "./SingleProduct";
 import "@/styles/ProductListing.min.css";
-import { Heading2 } from "../misc/Text";
+import {Heading2} from "../misc/Text";
+import AOSProvider from "../providers/AosProvider";
 function TshirtSection() {
 	return (
-		<div className='container w-full mx-auto my-10 px-4 md:px-12'>
-			<Heading2>T-SHIRTS</Heading2>
+		<AOSProvider>
+			<div className='container w-full mx-auto my-10 px-4 md:px-12'>
+				<Heading2>T-SHIRTS</Heading2>
 
-			<div className=' flex items-center justify-evenly flex-wrap  '>
-				{[...Array(4)].map((_, index) => (
-					<SingleProduct
-						key={index}
-						img={`/assets/img/products/tshirt${index + 1}.png`}
-						title='T-SHIRT'
-						price='120'
-						ratings='4.5'
-						link={`/shop/t-shirt/${index + 1}`}
-					/>
-				))}
+				<div className=' flex items-center justify-evenly flex-wrap  '>
+					{[...Array(4)].map((_, index) => (
+						<SingleProduct
+							key={index}
+							img={`/assets/img/products/tshirt${index + 1}.png`}
+							title='T-SHIRT'
+							price='120'
+							ratings='4.5'
+							link={`/shop/t-shirt/${index + 1}`}
+							data-aos='fade-up'
+						/>
+					))}
+				</div>
+				<button className='frame-36 '>
+					<span className='view-all'>View All</span>
+				</button>
+				<hr />
 			</div>
-			<button className='frame-36 '>
-				<span className='view-all'>View All</span>
-			</button>
-      <hr/>
-		</div>
+		</AOSProvider>
 	);
 }
 
