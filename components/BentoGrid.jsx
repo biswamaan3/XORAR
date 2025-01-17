1d5a48a542721596e5f92a1dd037ebd0b5359e48
Mayur Jadhav <mayurshrikantjadhav@gmail.com>
2025-01-17 21:59:33 +0530

    added bento card

--- components/BentoGrid.jsx
+++ components/BentoGrid.jsx
@@ -2,45 +2,99 @@ import React from "react";
 import "@/styles/BentoGrid.min.css";
 import Link from "next/link";
 import {BentoCardData} from "@/data/products";
+const divClasses =
+	"absolute inset-0 w-full h-full bg-black rounded-xl bg-opacity-50 ";
 
 function BentoGrid() {
 	return (
 		<>
-			<div className='main-section min-h-screen'>
+			<div className='main-section min-h-screen '>
 				{/* Title */}
 				<div className='style-browser'>
 					<h3 className='title-browse'>BROWSE BY DRESS STYLE</h3>
 
 					{/* Grid Layout */}
-					<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-6 py-8 px-6'>
-						{/* Generate Cards Dynamically */}
-						{BentoCardData.map((card, index) => (
-							<Link
-								key={index}
-								href={card.href}
-								className={`${card.colSpan} relative bg-cover bg-white bg-no-repeat rounded-xl`}
-								style={{
-									backgroundImage: `url(${card.backgroundImage})`,
-									backgroundSize: "cover",
-									backgroundPosition: "center",
-									height: "300px",
-								}}
-							>
-								<div className='absolute inset-0   '>
-									{/* Title on top of the image */}
-									<h3 className='title-bento'>
-										{card.title
-											.split("\n")
-											.map((line, idx) => (
-												<React.Fragment key={idx}>
-													{line}
-													<br />
-												</React.Fragment>
-											))}
-									</h3>
-								</div>
-							</Link>
-						))}
+					<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-12 gap-6 py-8 px-6 '>
+						{/* Card 1 */}
+						<Link
+							href={BentoCardData[0].href}
+							className='col-span-1 md:col-span-1 lg:col-span-5 relative bg-cover
+							
+							bg-no-repeat rounded-xl'
+							style={{
+								backgroundImage: `url(${BentoCardData[0].backgroundImage})`,
+								backgroundSize: "cover",
+								backgroundPosition: "center",
+								height: "300px",
+							}}
+						>
+							<div className={divClasses}>
+								{/* Title on top of the image */}
+								<h3 className='title-bento'>
+									{BentoCardData[0].title}
+								</h3>
+							</div>
+						</Link>
+
+						{/* Card 2 */}
+						<Link
+							href={BentoCardData[1].href}
+							className='col-span-1 md:col-span-1 lg:col-span-7 relative bg-cover bg-no-repeat rounded-xl'
+							style={{
+								backgroundColor: "white",
+								backgroundImage: `url(${BentoCardData[1].backgroundImage})`,
+								backgroundSize: "cover",
+								backgroundPosition: "center",
+								height: "300px",
+							}}
+						>
+							<div className={divClasses}>
+								{/* Title on top of the image */}
+								<h3 className='title-bento'>
+									{BentoCardData[1].title}
+								</h3>
+							</div>
+						</Link>
+
+						{/* Card 3 */}
+						<Link
+							href={BentoCardData[2].href}
+							className='col-span-1 md:col-span-1 lg:col-span-7 relative bg-cover bg-no-repeat rounded-xl'
+							style={{
+								backgroundColor: "white",
+								backgroundImage: `url(${BentoCardData[2].backgroundImage})`,
+								backgroundSize: "cover",
+								backgroundPosition: "center",
+								height: "300px",
+							}}
+						>
+							<div className={divClasses}>
+								{/* Title on top of the image */}
+								<h3 className='title-bento'>
+									{BentoCardData[2].title}
+								</h3>
+							</div>
+						</Link>
+
+						{/* Card 4 */}
+						<Link
+							href={BentoCardData[3].href}
+							className='col-span-1 md:col-span-1 lg:col-span-5 relative bg-cover bg-no-repeat rounded-xl'
+							style={{
+								backgroundColor: "white",
+								backgroundImage: `url(${BentoCardData[3].backgroundImage})`,
+								backgroundSize: "cover",
+								backgroundPosition: "center",
+								height: "300px",
+							}}
+						>
+							<div className={divClasses}>
+								{/* Title on top of the image */}
+								<h3 className='title-bento'>
+									{BentoCardData[3].title}
+								</h3>
+							</div>
+						</Link>
 					</div>
 				</div>
 			</div>
